415ed4a150f997d8edb73b8c7472ffa95e0735a7
Stone Liu <liu.sto@northeastern.edu>
2025-03-06 18:36:41 -0500

    Starting REPL

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 #include "./lib/file.h"
 
 int main(int argc, char* argv[]) {
-  // Read the file
+  if (argc == 1) {
+    // Start the REPL
+    printf("starting repl...");
+  } else if (argc == 2) {
+    // Assume they have given us a file path, read the file path.
+    printf("Compiling");
+  } else {
+    printf("%s", "Too many arguments given, correct usage ./a.out <FILE_PATH>");
+    return 1;
+  }
   return 0;
 }
