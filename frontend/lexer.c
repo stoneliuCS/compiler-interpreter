edbad1d59c722b5e5c564f0517298ad97272f610
Stone Liu <liu.sto@northeastern.edu>
2025-03-09 11:10:33 -0400

    A recognizer.

--- frontend/lexer.c
+++ frontend/lexer.c
@@ -1,13 +1,41 @@
 #include "lexer.h"
 #include <assert.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+static void parser(const char c);
+static void handle_alpha_numeric(const char c);
 
 GArray* tokenize(const char* source) {
   for (int i = 0; i < strlen(source); i++) {
     char c = source[i];
     if (c == ' ' || c == '\n') continue; //Skip over any whitespace or new lines.
-    printf("%c\n", c);
+    parser(c);
   }
   return NULL;
+}
+
+static void parser(const char c) {
+  switch (c) {
+    case SEMICOLON:
+      printf("FOUND SEMICOLON");
+      break;
+    case LEFT_PAREN:
+      printf("FOUND LEFT_PAREN");
+      break;
+    case RIGHT_PAREN:
+      printf("FOUND RIGHT_PAREN");
+      break;
+    default:
+      handle_alpha_numeric(c);
+  }
+}
+
+static void handle_alpha_numeric(char c) {
+  if (isalpha(c)){
+    printf("Is Alpha\n");
+  } else if (isdigit(c)) {
+    printf("is Digit\n");
+  }
 }
--- frontend/lexer.h
+++ frontend/lexer.h
@@ -4,6 +4,17 @@
 
 #define LEXER_H
 
+typedef enum STATEMENTS {
+  SEMICOLON = ';',
+  LEFT_PAREN = '(',
+  RIGHT_PAREN = ')'
+} STATEMENTS;
+
+typedef struct tokenizer_operations {
+
+} TokenizerOperations;
+
+// Tokenizes the source code into an Array of text.
 GArray* tokenize(const char* source);
 
 #endif
